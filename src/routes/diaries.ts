c664adeec1f7d32d76fb32bd388279d489c29cbb
Miguel Arias Moreno <arias.miguelm@gmail.com>
2023-03-04 02:10:45 -0500

    validation to post of visibility

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,33 +1,32 @@
 import express from 'express'
 import * as diaryServices from '../services/diaryServices'
-
+import toNewDiaryEntry from '../utils'
 
 const router = express.Router()
 
-router.get('/', (_req, res)=>{
-   res.send(diaryServices.getEntriesWithoutSensitiveInfo())
+router.get('/', (_req, res) => {
+    res.send(diaryServices.getEntriesWithoutSensitiveInfo())
 
 })
 
-router.get('/:id', (req, res)=>{
+router.get('/:id', (req, res) => {
     const diary = diaryServices.findByid(+req.params.id)
-    
-    return (diary !=null)
-      ? res.send(diary)
-      : res.sendStatus(404)
+
+    return (diary != null)
+        ? res.send(diary)
+        : res.sendStatus(404)
 })
 
-router.post('/', (req, res)=>{
-    const {date,weather,visibility,comment}= req.body
-    
-    const newDiaryEntry = diaryServices.addDiary({
-        date,
-        weather,
-        visibility,
-        comment
+router.post('/', (req, res) => {
+    try {
+      const newDiaryEntry = toNewDiaryEntry(req.body)
+  
+      const addedDiaryEntry = diaryServices.addDiary(newDiaryEntry)
+  
+      res.json(addedDiaryEntry)
+    } catch  {
+      res.status(400)
     }
-    )
-    res.json(newDiaryEntry)
 })
 
 export default router
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,16 @@
-export type Visibility = 'great' | 'good' | 'ok' | 'poor'
-
 export enum Weather {
     Sunny = 'sunny',
     Rainy = 'rainy',
     Cloudy = 'cloudy',
     Windy = 'windy',
     Stormy = 'stormy'
+}
+
+export enum Visibility {
+    Great = 'great',
+    Good = 'good',
+    Ok = 'ok',
+    Poor = 'poor'
 }
 
 export interface DiaryEntry{
